072353d5f8e09dd99f4c8781ec7f534a1d3523e0
Mingda Rui <mingdarui@gmail.com>
2019-03-11 23:40:02 +0000

    withPrintWriterV2 with currying

--- src/main/scala/program_in_scala/withPrintWriter.scala
+++ src/main/scala/program_in_scala/withPrintWriter.scala
@@ -10,10 +10,26 @@ object WithPrintWriter {
         }
     }
 
+    // currying 
+    def withPrintWriterV2(file: File)(op: PrintWriter => Unit) = {
+        val writer = new PrintWriter(file)
+        try {
+            op(writer)
+        } finally {
+            writer.close()
+        }
+    }
+
     def main(args: Array[String]): Unit = {
         withPrintWriter(
             new File("date.txt"),
             writer => writer.println(new java.util.Date)
         )
+
+        // currying
+        val file = new File("date.txt")
+        withPrintWriterV2(file) { writer => 
+          writer.println(new java.util.Date)
+        }
     }
 }

767c08c192acc11bdd64a8d93e0c7c8f9b367cbe
Mingda Rui <mingdarui@gmail.com>
2019-03-11 21:56:27 +0000

    containsNeg in higher order function

--- src/main/scala/program_in_scala/containsNeg.scala
+++ src/main/scala/program_in_scala/containsNeg.scala
@@ -9,7 +9,11 @@ object containsNeg {
         exists
     }
 
+    // Use higher order function
+    def containsNegV2(nums: List[Int]) = nums.exists(_ < 0)
+
     def main(args: Array[String]): Unit = {
         println(containsNeg(List(1,2,3,4)))
+        println(containsNegV2(List(1,2,-3,4)))
     }
 }

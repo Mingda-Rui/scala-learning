30b4779717918a5c2e00d357294dd3dc52770de7
Mingda Rui <mingdarui@gmail.com>
2019-03-12 19:25:42 +0000

    ArrayElement

--- src/main/scala/program_in_scala/chpt10/Element.scala
+++ src/main/scala/program_in_scala/chpt10/Element.scala
@@ -1,8 +1,12 @@
 package mingda.scala.learning.chpt10
 
-  abstract class Element {
-    def contents: Array[String]
-    val height = contents.length
-    val width =
-      if (height == 0) 0 else contents(0).length    
-  } 
+abstract class Element {
+  def contents: Array[String]
+  val height = contents.length
+  val width =
+    if (height == 0) 0 else contents(0).length    
+} 
+
+class ArrayElement(conts: Array[String]) extends Element {
+    def contents: Array[String] = conts
+}

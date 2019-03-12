49edae4ec81a2cc05ff9ef9c7041b9a02bfe661b
Mingda Rui <mingdarui@gmail.com>
2019-03-12 19:36:32 +0000

    Element Test bug fix

--- src/test/scala/chpt10/ElementTest.scala
+++ src/test/scala/chpt10/ElementTest.scala
@@ -4,5 +4,8 @@ import mingda.scala.learning.chpt10.ArrayElement
 object ElementTest {
     def main(args: Array[String]): Unit = {
         val e: Element = new ArrayElement(Array("hello"))
+        for(s <- e.contents) {
+            println(s)
+        }
     }
 }

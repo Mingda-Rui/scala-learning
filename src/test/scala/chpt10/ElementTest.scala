ff8a55e5eedf1440aed4b87bdabd914e350c8e7d
Mingda Rui <mingdarui@gmail.com>
2019-03-12 19:29:53 +0000

    Element Test

---
+++ src/test/scala/chpt10/ElementTest.scala
@@ -0,0 +1,8 @@
+import mingda.scala.learning.chpt10.Element
+import mingda.scala.learning.chpt10.ArrayElement
+
+object ElementTest {
+    def main(args: Array[String]): Unit = {
+        val e: Element = new ArrayElement(Array("hello"))
+    }
+}

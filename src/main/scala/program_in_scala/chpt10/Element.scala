ddbecafef7764ff464f4a4ba10efc8a4e5950c6b
Mingda Rui <mingdarui@gmail.com>
2019-03-13 12:17:16 +0000

    beside functionality

--- src/main/scala/program_in_scala/chpt10/Element.scala
+++ src/main/scala/program_in_scala/chpt10/Element.scala
@@ -18,6 +18,15 @@ abstract class Element {
 
   def above(that: Element): Element =
     new ArrayElement(this.contents ++ that.contents)
+
+  def beside(that: Element): Element =
+    new ArrayElement(
+      for (
+        (line1, line2) <- this.contents zip that.contents
+      ) yield line1 + line2
+    )
+
+  override def toString = contents mkString "\n"
 } 
 
 // Original ArrayElement

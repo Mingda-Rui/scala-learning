a983c40755255afc03a998be0246fb5a6b7fdb85
Mingda Rui <mingdarui@gmail.com>
2019-03-22 19:00:56 +0000

    self defined value class SwissFrancs

--- src/main/scala/program_in_scala/chpt11/Dollars.scala
+++ src/main/scala/program_in_scala/chpt11/Dollars.scala
@@ -2,6 +2,12 @@ class Dollars(val amount: Int) extends AnyVal {
   override def toString() = "$" + amount
 }
 
+class SwissFrancs(val amount: Int) extends AnyVal {
+  override def toString() = amount + " CHF"
+}
+
 object Dollars {
   val money = new Dollars(10000000)
+  val dollars = new Dollars(1000)
+  val francs = new SwissFrancs(1000)
 }

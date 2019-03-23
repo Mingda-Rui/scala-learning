0b48a6406b0ab4eeb2ed2ef8cadfb26ffcbaa94d
Mingda Rui <mingdarui@gmail.com>
2019-03-23 13:35:45 +0000

    SlowAppendQueueV2

--- src/main/scala/program_in_scala/chpt19/SlowAppendQueue.scala
+++ src/main/scala/program_in_scala/chpt19/SlowAppendQueue.scala
@@ -3,3 +3,10 @@ class SlowAppendQueue[T](elems: List[T]) { // Not efficient
   def tail = new SlowAppendQueue(elems.tail)
   def enqueue(x: T) = new SlowAppendQueue(elems ::: List(x))
 }
+
+class SlowAppendQueueV2[T](smele: List[T]) { // Not efficient
+  // smele is elems reversed 
+  def head = smele.last
+  def tail = new SlowAppendQueueV2(smele.init)
+  def enqueue(x: T) = new SlowAppendQueueV2(x :: smele)
+}

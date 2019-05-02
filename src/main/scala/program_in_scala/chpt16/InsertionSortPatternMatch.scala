0c8133ba4c27df3bb72df71fcc6748c574496a5b
Mingda Rui <mingdarui@gmail.com>
2019-05-02 23:16:09 +0100

    insertion sort with pattern matching

---
+++ src/main/scala/program_in_scala/chpt16/InsertionSortPatternMatch.scala
@@ -0,0 +1,10 @@
+object InsertionSortPatternMatch {
+  def isort(xs: List[Int]): List[Int] = xs match {
+    case List() => List()
+    case x :: xs1 => insert(x, isort(xs1))
+  }
+  def insert(x:Int, xs: List[Int]): List[Int] = xs match {
+    case List() => List(x)
+    case y :: ys => if (x <= y) x :: xs else y :: insert(x, ys)
+  }
+}
